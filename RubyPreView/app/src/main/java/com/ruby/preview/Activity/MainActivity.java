65d599d382b3a87d7daf4e739ec27dfac371b6bc
NieJianJian <niejianjian_sir@163.com>
2016-12-02 15:35:01 +0800

    增加删除文件功能

--- RubyPreView/app/src/main/java/com/ruby/preview/Activity/MainActivity.java
+++ RubyPreView/app/src/main/java/com/ruby/preview/Activity/MainActivity.java
@@ -4,18 +4,48 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.ruby.preview.R;
+import com.ruby.preview.utils.GetFileUtil;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
-    private Button mFansBtn, mPreviewBtn;
+    //    private Button mDeleteBtn, mSelectBtn;
+    private ListView mListView;
+    private final static String QQPATH = File.separatorChar + "Tencent" + File.separatorChar + "QQfile_recv";
+    private List<String> mFileList, mFileNameList;
+    private ArrayAdapter<String> mAdapter;
+    private String[] urlBuff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFileNameList = new ArrayList<>();
+        mFileList = new ArrayList<>();
+        mListView = (ListView) findViewById(R.id.main_listview);
+
+        mFileList = GetFileUtil.getFileList(QQPATH, ".xls", true);
+        if (mFileList.size() > 0) {
+            for (String s : mFileList) {
+                String name = s.split("\\/")[s.split("\\/").length - 1];
+                mFileNameList.add(name);
+            }
+            urlBuff = mFileNameList.toArray(new String[mFileList.size()]);
+//                        String[] urlBuff = (String[]) mFileNameList.toArray(new String[0]);
+            mAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, urlBuff);
+            mListView.setAdapter(mAdapter);
+        } else {
+            mListView.setVisibility(View.GONE);
+        }
+
     }
 
     public void fansClick(View view) {
@@ -26,6 +56,16 @@ public class MainActivity extends Activity {
     public void previewClick(View view) {
         Intent intent = new Intent(MainActivity.this, PreviewActivity.class);
         startActivity(intent);
+    }
+
+    public void deleteClick(View view) {
+        for (String path : mFileList) {
+            File file = new File(path);
+            if (file.exists()) {
+                file.delete();
+            }
+        }
+        mListView.setVisibility(View.GONE);
     }
 
 }
